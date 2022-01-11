af7dcca0cbd97618c5f721a5fe7543a3b35df8af
SGI12 <zaretskiy03@gmail.com>
2022-01-11 15:03:16 +0300

    Webpack is installed

---
+++ src/index.js
@@ -0,0 +1,1 @@
+import './main/components/button/button.scss'
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HTMLWebpackPugPlugin = require('html-webpack-pug-plugin');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        main: "./index.js",
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'img/[name][ext]'
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./main/pages/index.pug",
+            filename: "index.html",
+            minify: false
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        }),
+        new HTMLWebpackPugPlugin()
+    ],
+ module: {
+     rules:
+     [
+         {
+             test: /\.scss$/,
+             use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+         },
+         {
+             test: /\.(png|jpg|svg|gif)$/,
+             type: 'asset/resource'
+         },
+         {
+             test: /\.pug$/,
+             loader: 'pug-loader'
+         }
+     ]
+ }
+}
