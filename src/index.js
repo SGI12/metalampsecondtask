486d459d8b9a30405b9c060ec1adc2ae22155db9
SGI12 <zaretskiy03@gmail.com>
2022-01-17 17:09:38 +0300

    finished the buttons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './main/components/button/button.scss'
 import '../node_modules/normalize.css/normalize.css'
+import './main/components/button/button.scss'
 import './styles/vars.scss'
 import './fonts/fonts.scss'
